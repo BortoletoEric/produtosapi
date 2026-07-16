6910e200d6a89919b992e7c25f20664e99972269
Bortoleto <ericbortoleto_9@outlook.com>
2026-07-16 16:22:27 -0300

    Refactor Produto model with JPA and Lombok and add GET and DELETE endpoints to ProdutoController

--- src/main/java/io/github/bortoletoeric/produtosapi/controler/ProdutoControler.java
+++ src/main/java/io/github/bortoletoeric/produtosapi/controler/ProdutoControler.java
@@ -1,19 +1,39 @@
 package io.github.bortoletoeric.produtosapi.controler;
 
 import io.github.bortoletoeric.produtosapi.model.Produto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.bortoletoeric.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoControler {
 
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoControler(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto recebido: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto buscarPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletarPorId(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
     }
 
 }
