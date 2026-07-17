7c5277497196d574e203d9ec48e6b6521f386e0a
Bortoleto <ericbortoleto_9@outlook.com>
2026-07-17 12:01:30 -0300

    Add findByNome method to ProdutoRepository and GET endpoint to ProdutoControler for searching by name

--- src/main/java/io/github/bortoletoeric/produtosapi/controler/ProdutoControler.java
+++ src/main/java/io/github/bortoletoeric/produtosapi/controler/ProdutoControler.java
@@ -4,6 +4,7 @@ import io.github.bortoletoeric.produtosapi.model.Produto;
 import io.github.bortoletoeric.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -43,5 +44,10 @@ public class ProdutoControler {
         produto.setId(id);
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping
+    public List<Produto> buscarPorNome(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNome(nome);
     }
 }
--- src/main/java/io/github/bortoletoeric/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/io/github/bortoletoeric/produtosapi/repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package io.github.bortoletoeric.produtosapi.repository;
 import io.github.bortoletoeric.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
-    
+    List<Produto> findByNome(String nome);
 }
